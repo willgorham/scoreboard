81b07aa36eba4e7cbdbbf055d805b6d247b3380b
Will Gorham <willgorham@gmail.com>
2019-01-03 18:58:41 -0500

    Fix issues with leaderboard / icon

--- src/components/App.js
+++ src/components/App.js
@@ -10,25 +10,21 @@ class App extends Component {
         name: "Guil",
         id: 1,
         score: 0,
-        isLeader: false,
       },
       {
         name: "Treasure",
         id: 2,
         score: 0,
-        isLeader: false,
       },
       {
         name: "Ashley",
         id: 3,
         score: 0,
-        isLeader: false,
       },
       {
         name: "James",
         id: 4,
         score: 0,
-        isLeader: false,
       }
     ]
   };
@@ -42,21 +38,13 @@ class App extends Component {
   }
 
   handleUpdateScore = (id, scoreDelta) => {
-    this.setState( state => {
-      const newScore = state.players.find(p => p.id === id).score + scoreDelta
-      const highScore = Math.max(newScore, ...state.players.map(p => p.score));
+    this.setState( prevState => {
+      const players = prevState.players.map( p => {
+        p.id === id && (p.score = p.score + scoreDelta);
+        return p;
+      });
 
-      return {
-        players: state.players.map( p => {
-          if (p.id === id) {
-            p.score = newScore;
-          }
-
-          p.isLeader = p.score === highScore;
-
-          return p;
-        }),
-      };
+      return { players };
     });
   }
 
@@ -76,7 +64,14 @@ class App extends Component {
     });
   }
 
+  getHighScore() {
+    const highScore = Math.max(...this.state.players.map(player => player.score));
+    return highScore > 0 ? highScore : null;
+  }
+
   render() {
+    const highScore = this.getHighScore();
+
     return (
       <div className="scoreboard">
         <Header
@@ -91,7 +86,7 @@ class App extends Component {
             id={ player.id }
             key={ player.id.toString() }
             score={ player.score }
-            isLeader={ player.isLeader }
+            isLeader={ highScore === player.score }
             removePlayer={ this.handleRemovePlayer }
             updateScore={ this.handleUpdateScore }
           />
