8fac0747a77e53bba5b6a6807f317bbefc28c9ad
Will Gorham <willgorham@gmail.com>
2019-01-11 20:23:51 -0500

    Updates

---
+++ precache-manifest.979062d2d6bb4f0cda88b72d6cb6083d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "487eaab11f24ca9c9483",
+    "url": "/scoreboard/static/css/main.89f4e8a1.chunk.css"
+  },
+  {
+    "revision": "487eaab11f24ca9c9483",
+    "url": "/scoreboard/static/js/main.487eaab1.chunk.js"
+  },
+  {
+    "revision": "9ef807963481eb41277b",
+    "url": "/scoreboard/static/js/1.9ef80796.chunk.js"
+  },
+  {
+    "revision": "dc1b41325083169089dd",
+    "url": "/scoreboard/static/js/runtime~main.dc1b4132.js"
+  },
+  {
+    "revision": "e3b03446c064001aabfc521e2f690d7b",
+    "url": "/scoreboard/index.html"
+  }
+];
